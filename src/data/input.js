d4417cbcad85de050e8973f1226907781a47d584
M-Akhtar <muhammad.akhtar@acqu.co>
2023-02-21 18:39:02 +0500

    initial commit

---
+++ src/data/input.js
@@ -0,0 +1,5 @@
+{
+  "ts": 1234567890,
+  "u": "https://www.test.com/products/productA.html?a=5435&b=test#reviews",
+  "e": [{"et":"dl","n":"utag_data","u":{"page_name":"store","store_id":115}},{"et":"ur"}]
+}
---
+++ src/data/output.js
@@ -0,0 +1,26 @@
+[{
+  "timestamp": 1234567890,
+  "url_object": {
+    "domain": "www.test.com"
+    "path": "/products/productA.html"
+    "query_object": {
+			"a": "5435",
+			"b": "test",
+		}
+    "hash": "#reviews"
+  }
+  "ec": {"et":"dl","n":"utag_data","u":{"page_name":"store","store_id":115}}
+},
+{
+  "timestamp": 1234567890,
+  "url_object": {
+    "domain": "www.test.com"
+    "path": "/products/productA.html"
+    "query_object": {
+			"a": "5435",
+			"b": "test",
+		}
+    "hash": "#reviews"
+  }
+  "ec": {"et":"ur"}
+}]
